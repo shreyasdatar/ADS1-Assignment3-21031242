0e03ac4a88d897496aa07714d3f4f170f43bdea9
shreyasdatar <101811543+shreyasdatar@users.noreply.github.com>
2022-05-12 16:30:28 +0100

    Add files via upload

--- Assignment3.py
+++ Assignment3.py
@@ -8,7 +8,32 @@ Created on Thu May 12 10:28:57 2022
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 import sklearn.cluster as cluster
+from err_ranges import err_ranges
+
+def growthfunct(t, s, k):
+    '''
+    
+
+    Parameters
+    ----------
+    t : int
+        Year
+    s : int
+        population
+    k : float
+        Growth rate
+
+    Returns
+    -------
+    op : TYPE
+        DESCRIPTION.
+
+    '''
+    op = s * np.exp(k * (t - 2008))
+    
+    return op
 
 def read_data(fname):
     """
@@ -79,9 +104,52 @@ def makeplot(df, col1, col2):
     plt.xlabel(col1)
     plt.ylabel(col2)
     plt.show()
+
+#KMeans Clusteing
+def kmeans(df,x,y):
+    '''
+    The function plots KMeans Clusters.
+
+    Parameters
+    ----------
+    df : Dataframe
+        Dataframe from which data is to be taken.
+    x : String
+        1st column to be considered for clustering.
+    y : String
+        2nd column to be considered for clustering.
+
+    Returns
+    -------
+    None.
+
+    '''
+    xlab = x
+    ylab = y
+    x = df[x]
+    y = df[y]
+
+    kmeans = cluster.KMeans(n_clusters=4)
+    kmeans.fit(df)
+
+    labels = kmeans.labels_
+    cen = kmeans.cluster_centers_
+
+    plt.figure()
+    col = ["red", "green", "blue", "yellow"]
+    for l in range (0,4):
+        plt.plot(x[labels==l], y[labels==l], "o", markersize=3, color=col[l])
+
+    for ic in range(4):
+        xc, yc = cen[ic,:]
+        plt.plot(xc, yc, "dk", markersize=10)
+    plt.title("KMeans Clustering")
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)    
+
     
-    
-df_cc, fd_ccT = read_data("Dataset.csv")
+df_cc, df_ccT = read_data("Dataset.csv")
+df_ind = df_cc[df_cc["Country Name"] == "India"]
 print(df_cc.describe())
 print(df_cc.corr())
 print(df_cc.head())
@@ -90,24 +158,40 @@ for col in df_cc.columns[2:]:
     df_cc[col] = norm(df_cc[col])
 
 
-df_CO_TP = df_cc[["CO2 emissions (metric tons per capita)", "Population total"]].copy()
+df_CO_TP = df_cc[["CO2 emissions (metric tons per capita)", 
+                  "Population total"]].copy()
+kmeans(df_CO_TP,"CO2 emissions (metric tons per capita)","Population total")
 
-#KMeans Clusteing
-x = df_CO_TP["CO2 emissions (metric tons per capita)"]
-y = df_CO_TP["Population total"]
+df_UP_TP = df_cc[["Urban population",
+                  "Population total"]].copy()
+kmeans(df_UP_TP,"Urban population","Population total")
 
-kmeans = cluster.KMeans(n_clusters=4)
-kmeans.fit(df_CO_TP)
+df_PG_TP = df_cc[["Population growth (annual %)",
+                  "Population total"]].copy()
+kmeans(df_PG_TP,"Population growth (annual %)","Population total")
 
-labels = kmeans.labels_
-cen = kmeans.cluster_centers_
+df_ind = df_cc[df_cc["Country Name"] == "India"]
+
+param, pcovar = opt.curve_fit(growthfunct, df_ind["Time"], 
+                              df_ind["Population total"])
+
+sigma = np.sqrt(np.diag(pcovar))
+low, up = err_ranges(df_ind["Time"], growthfunct, param, sigma)
+
+print(param, pcovar, sep="\n")
+
+df_ind["pop_exp"] = growthfunct(df_ind["Time"],*param)
 
 plt.figure()
-col = ["red", "green", "blue", "yellow"]
-for l in range (0,4):
-    plt.plot(x[labels==l], y[labels==l], "o", markersize=3, color=col[l])
+plt.plot(df_ind["Time"], df_ind["Population total"], label="data")
+plt.plot(df_ind["Time"], df_ind["pop_exp"], label="fit")
+plt.fill_between(df_ind["Time"], low, up, alpha=0.7)
+plt.xlabel("Time")
+plt.ylabel("Population")
+plt.title("")
+plt.legend()
 
-for ic in range(4):
-    xc, yc = cen[ic,:]
-    plt.plot(xc, yc, "dk", markersize=10)
-plt.title("KMeans Clustering")
+print("Forcasted population")
+low30, up30 = err_ranges(2030, growthfunct, param, sigma)
+print("2030 between ", low30, "and", up30)
+
